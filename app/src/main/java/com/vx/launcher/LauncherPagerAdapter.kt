5ae1b0248b57cda63ffebb1aad08c41b2f9706d2
kiua0099-sketch <kiua0099@gmail.com>
2026-06-25 16:02:05 +0300

    Add LauncherPagerAdapter for fragment management

---
+++ app/src/main/java/com/vx/launcher/LauncherPagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.vx.launcher
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class LauncherPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> HomeFragment()
+            1 -> ModMarketFragment()
+            2 -> SettingsFragment()
+            else -> HomeFragment()
+        }
+    }
+}
